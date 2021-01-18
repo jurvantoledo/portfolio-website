957e3be5bb07d0894b18f5c68f950de06d487daa
jurvantoledo <jurvantoledo@live.nl>
2021-01-18 16:25:47 +0100

    made bootstrap work

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,13 +1,18 @@
 import React from "react"
+import { Button, Container, Jumbotron } from "react-bootstrap"
 import About from "../About"
 
 
 export default function Homepage() {
     return (
     <>
-        <div>
-            Hello
+        <div className="banner">
         </div>
+        <Jumbotron>
+                <h1>Jur van Toledo</h1>
+        </Jumbotron>
+        <Container>
+        </Container>
         <About />
     </>
     )
