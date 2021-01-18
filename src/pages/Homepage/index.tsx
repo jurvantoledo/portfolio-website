5fc0cb3c5e99facf01531f02b9d77b9e5b3b21c0
jurvantoledo <jurvantoledo@live.nl>
2021-01-18 16:56:07 +0100

    gave banner some height

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,12 +1,19 @@
 import React from "react"
-import { Button, Container, Jumbotron } from "react-bootstrap"
+import { 
+    Button, 
+    Container, 
+    Jumbotron 
+} from "react-bootstrap"
 import About from "../About"
 
 
 export default function Homepage() {
     return (
     <>
-        <div className="banner">
+        <div 
+        className="banner"
+        style={{ backgroundImage: `url(https://images.pexels.com/photos/1036857/pexels-photo-1036857.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)` }}
+        >
         </div>
         <Jumbotron>
                 <h1>Jur van Toledo</h1>
