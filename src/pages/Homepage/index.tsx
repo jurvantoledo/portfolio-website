755b2505c9c97d51f348471acaa3234358bd8f16
jurvantoledo <jurvantoledo@live.nl>
2021-01-18 16:09:30 +0100

    first commit

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import About from "../About"
+
+
+export default function Homepage() {
+    return (
+    <>
+        <div>
+            Hello
+        </div>
+        <About />
+    </>
+    )
+}
