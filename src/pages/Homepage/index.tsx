4e2ae69c9475fcc254946f091557f21d44914292
jurvantoledo <jurvantoledo@live.nl>
2021-01-21 11:19:03 +0100

    changed layout

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { 
-    Button, 
     Container, 
     Jumbotron 
 } from "react-bootstrap"
@@ -15,19 +14,9 @@ export default function Homepage() {
         className="banner-container"
         style={{ backgroundImage: `url(https://images.pexels.com/photos/1036857/pexels-photo-1036857.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)` }}
         >
+            <h1>Hi I'm Jur van Toledo</h1>
         </div>
-        <Jumbotron>
-                <h1>Jur van Toledo</h1>
-        </Jumbotron>
-        <Container>
-            <p>
-                Hi my name is Jur van Toledo and I'm a 21 year old 
-                Junior developer from The Netherlands.
-                Recently I graduated at the Codaisseur's Academy as Fullstack Developer, 
-                passionated in creating awesome web- and mobile applications. 
-                I am an enthusiastic developer looking for a new challenge to develop/learn more skills.
-            </p>
-        </Container>    
+ 
         <About />
     </>
     )
