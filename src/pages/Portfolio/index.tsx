b6f16c5b67b55a397b5cc780048bcf28d6865eef
jurvantoledo <jurvantoledo@live.nl>
2021-01-22 13:00:09 +0100

    added portfolio project

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -2,8 +2,11 @@ import React from "react"
 import { 
     Container, 
     Jumbotron, 
-    Col
+    Col,
+    Card
 } from "react-bootstrap"
+
+import "./portfolio.scss"
 
 export default function Portfolio() {
     return (
@@ -16,10 +19,44 @@ export default function Portfolio() {
         <Jumbotron className="title">
             <h2>Portfolio</h2>
         </Jumbotron>
-        <Container className="prortfolio-container">
+        <Container className="portfolio-container">
             <p>
               <strong>Projects</strong>
             </p>
+            <Container 
+            className="portfolio-projects"
+            as={Col} 
+            md={{ span: 12}}
+            >
+                <Card 
+                as={Col} 
+                md={{ span: 3}}
+                >
+                 <figure>
+                   <img src="https://images.pexels.com/photos/2974623/pexels-photo-2974623.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="project-1" />
+                 </figure>
+                 <a target="_blank" 
+                 href="https://add-that-store.netlify.app/"
+                 >
+                    Add A Store
+                </a>
+                </Card>
+                <Card 
+                as={Col} 
+                md={{ span: 3}}
+                >
+                 <figure>
+                   <img src="https://images.pexels.com/photos/2974623/pexels-photo-2974623.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="project-1" />
+                 </figure>
+                 <a 
+                 target="_blank" 
+                 href="https://add-that-store.netlify.app/"
+                 >
+                    Add A Store
+                </a>
+                </Card>
+
+            </Container>
         </Container>
       </Container>
     </>
