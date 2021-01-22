070325a319808cac60f01344b0e9327042d45548
jurvantoledo <jurvantoledo@live.nl>
2021-01-22 12:29:46 +0100

    added text

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,9 +1,17 @@
 import React from "react"
+import { Container, Jumbotron } from "react-bootstrap"
 
 export default function Portfolio() {
     return (
     <>
-        Hello Portfolio page
+        <Jumbotron className="title">
+            <h2>Portfolio</h2>
+        </Jumbotron>
+        <Container className="prortfolio-container">
+            <p>
+              <strong>Projects</strong>
+            </p>
+        </Container>
     </>
     )
 }
