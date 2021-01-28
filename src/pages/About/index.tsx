52ab11383caebdc092fac0b05d03917087f12e58
jurvantoledo <jurvantoledo@live.nl>
2021-01-28 18:20:13 +0100

    added logos from learned technologies

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { 
     Card, 
     Container, 
-    Jumbotron,
     Col, 
 } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,11 +10,19 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 import ReactLogo from "./Logo/react-logo.png"
 import ReduxLogo from "./Logo/redux-logo.png"
+import PostgresLogo from "./Logo/postgresQL-logo.png"
+import NodeJSLogo from "./Logo/nodeJS-logo.png"
+import HtmlLogo from "./Logo/html-logo.png"
+import CssLogo from "./Logo/css-logo.png"
+import JsLogo from "./Logo/JavaScript_logo.png"
+import TsLogo from "./Logo/Typescript_logo_2020.png"
+import ScssLogo from "./Logo/sass-logo.png"
+import ExpressLogo from "./Logo/express-logo.png"
+import GitLogo from "./Logo/git-logo.png"
 
 
-import "./about.scss"
 import Foto from "./foto-jur.jpg"
-
+import "./about.scss"
 
 export default function About() {
     return(
@@ -41,16 +48,20 @@ export default function About() {
             as={Col} 
             md={{ span: 7 }}
             >
+            <div className="about-me-title">
               <h3>
                 About Me <br/>
               <small>Junior Full-Stack Developer</small>
               </h3>
+            </div>
                 <p>
                     I'm <strong>Jur van Toledo</strong> a former truck driver trying to become a developer.
-                    recently I graduated at the Codaisseur's Academy as Fullstack Developer, 
+                    Recently I graduated at the Codaisseur's Academy as Fullstack Developer, 
                     passionated in creating awesome web- and mobile applications. 
                     I am an enthusiastic developer looking for a new challenge to develop/learn more skills.
                 </p>
+                <p><strong>Email:</strong> <a href="mailto:jurvantoledo@live.nl">jurvantoledo@live.nl</a></p>
+                <p><strong>Phone:</strong> 06 51867733</p>
             <div className="social-icons">
             <h4>Socials</h4>
              <a target="_blank" rel="noreferrer" href="https://github.com/jurvantoledo">
@@ -62,12 +73,32 @@ export default function About() {
             </div>
             </Card>
            </Container>
+        </Container>
+        <Container className="techContainer">
            <h3>Technologies</h3>
            <Container
             as={Col} 
             md={{ span: 12}}
             className="techStack"
            >
+           <Card
+            as={Col} 
+            md={{ span: 2 }}
+            className="technology"
+           >
+            <figure>
+               <img src={JsLogo} alt="JavaScript" />
+            </figure>
+           </Card>
+           <Card
+            as={Col} 
+            md={{ span: 2 }}
+            className="technology"
+           >
+            <figure>
+               <img src={TsLogo} alt="TypeScript" />
+            </figure>
+           </Card>
            <Card
              as={Col} 
              md={{ span: 2 }}
@@ -86,8 +117,71 @@ export default function About() {
                <img src={ReduxLogo} alt="redux" />
             </figure>
            </Card>
+           <Card
+             as={Col} 
+             md={{ span: 2 }}
+             className="technology"
+           >
+             <figure>
+               <img src={HtmlLogo} alt="HTML" />
+            </figure>
+           </Card>
+           <Card
+             as={Col} 
+             md={{ span: 2 }}
+             className="technology"
+           >
+             <figure>
+               <img src={CssLogo} alt="CSS" />
+            </figure>
+           </Card>
+           <Card
+             as={Col} 
+             md={{ span: 2 }}
+             className="technology"
+           >
+             <figure>
+               <img src={ScssLogo} alt="Scss" />
+            </figure>
+           </Card>
+           <Card
+             as={Col} 
+             md={{ span: 2 }}
+             className="technology"
+           >
+             <figure>
+               <img src={PostgresLogo} alt="postgresQl" />
+            </figure>
+           </Card>
+           <Card
+             as={Col} 
+             md={{ span: 2 }}
+             className="technology"
+           >
+             <figure>
+               <img src={ExpressLogo} alt="express" />
+            </figure>
+           </Card>
+           <Card
+             as={Col} 
+             md={{ span: 2 }}
+             className="technology"
+           >
+             <figure>
+               <img src={NodeJSLogo} alt="nodeJS" />
+            </figure>
+           </Card>
+           <Card
+             as={Col} 
+             md={{ span: 2 }}
+             className="technology"
+           >
+             <figure>
+               <img src={GitLogo} alt="nodeJS" />
+            </figure>
+           </Card>
            </Container>
-        </Container>
+          </Container>
         </>
     )
 }
